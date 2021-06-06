d005b956e53f3f933caa2bb49e156931c6302a18
azharkalarickal <azharkka@gmail.com>
2021-06-06 21:39:57 +0530

    Updates

--- static/js/main.46c0ea89.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),a=n(3),s=n.n(a),p=n(0);var i=function(){return Object(p.jsx)("div",{className:"App",children:"azhar"})};s.a.render(Object(p.jsx)(r.a.StrictMode,{children:Object(p.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.46c0ea89.chunk.js.map
