1ff502d040a66ff4400471bd1d1edde828ab7ae0
azharkalarickal <azharkka@gmail.com>
2021-06-06 19:42:28 +0530

    Updates

---
+++ static/js/main.d30bf679.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{9:function(s,e,t){"use strict";t.r(e);var n=t(1),c=t.n(n),r=t(3),p=t.n(r),a=t(0);var d=function(){return Object(a.jsx)("div",{className:"App",children:"fdsghsfdgsdfghsfghsfhsfhs"})};p.a.render(Object(a.jsx)(c.a.StrictMode,{children:Object(a.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.d30bf679.chunk.js.map
